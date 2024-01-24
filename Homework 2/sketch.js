0b8dd2eef062de0247a98ec98aff200649df0c2b
dylan <dylan.kidd@umontana.edu>
2024-01-24 15:49:37 -0700

    homework 2

---
+++ Homework 2/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(170,220,310);
+  fill(223,123,0)
+  circle(120,280,150);
+  fill(80,80,200)
+  circle(120,80,30);
+  circle(170,95,30);
+  circle(140,110,30);
+  fill(255, 255, 110)
+  ellipse(260,140,90,70);
+  fill(680,30,20)
+  circle(270,280,30);
+  circle(310,280,30);
+  line(310, 268, 290, 220);
+  line(270, 268, 290, 220);
+}
